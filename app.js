bde6b96a40b218b374f218fffb2b838ceecd8d2b
Shi Tao Luo <sluo5991@gmail.com>
2021-12-31 09:40:58 -0500

    create a error page

--- app.js
+++ app.js
@@ -53,8 +53,9 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground)
+    if (!req.body.campground) {
       throw new ExpressError('Invalid Campground Data', 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -107,8 +108,11 @@ app.all('*', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Something went wrong' } = err;
-  res.status(statusCode).send(message);
+  const { statusCode = 500 } = err;
+  if (!err.message) {
+    err.message = 'Something Went Wrong';
+  }
+  res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {

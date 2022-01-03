73328f04084b85b6574109f016fd6a15cbadcc51
Shi Tao Luo <sluo5991@gmail.com>
2022-01-03 11:05:45 -0500

    implement delete review

--- app.js
+++ app.js
@@ -143,6 +143,16 @@ app.post(
   })
 );
 
+app.delete(
+  '/campgrounds/:id/reviews/:reviewId',
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
 // Throw a error page for all paths that does not exist
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404));

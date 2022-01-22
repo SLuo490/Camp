7904eeee37e10ad43bbd6e34c996a34f200e949c
Shi Tao Luo <sluo5991@gmail.com>
2022-01-22 09:18:26 -0500

    add upload image to edit page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,9 +25,10 @@ router
   .route('/:id')
   .get(catchAsync(campgrounds.showCampground))
   .put(
-    validateCampground,
     isLoggedIn,
     isAuthor,
+    upload.array('image'),
+    validateCampground,
     catchAsync(campgrounds.updateCampground)
   )
   .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
